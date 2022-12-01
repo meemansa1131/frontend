cd99e5d1a5968f23b2b60325b18d03efaaf16e9e
meemansa1131 <meemansawalia@gmail.com>
2022-12-01 21:19:53 +0530

    second

---
+++ src/components/Footer.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+const Footer = () => (
+    <footer>
+        Created by <span id="rajat">Meemansa</span>
+        <br />
+        <span>
+            <i
+                className="fab fa-github"
+                onClick={() =>
+                    window.open("https://github.com/meemansa1131", "_blank")
+                }
+            ></i>{" "}
+            {/* <i
+                className="fab fa-hackerrank"
+                onClick={() =>
+                    window.open("", "_blank")
+                }
+            ></i>{" "} */}
+            {/* <i
+                className="fab fa-linkedin"
+                onClick={() =>
+                    window.open(
+                        "",
+                        "_blank"
+                    )
+                }
+            ></i>{" "} */}
+            {/* <i
+                className="fab fa-stack-overflow"
+                onClick={() =>
+                    window.open(
+                        "",
+                        "_blank"
+                    )
+                }
+            ></i>{" "} */}
+            {/* <i
+                className="fab fa-twitter"
+                onClick={() =>
+                    window.open("", "_blank")
+                }
+            ></i> */}
+        </span>
+    </footer>
+);
+
+export default Footer;
